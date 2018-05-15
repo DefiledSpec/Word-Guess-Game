deb43070b510bb69fe0817fdfc369a7ac9106091
defiledspec <harrahjz@gmail.com>
2018-05-15 16:22:07 -0500

    Added Word Class, added start function, added displayText function, some index.html changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,8 @@
-document.write(`Press any key to get started.`);
-const pBreak = `<br>`;
-document.write(pBreak);
 
-let running = false;
 
-let words = [
+// Variables
+let word;
+let wordList = [
     'blue',
     'red',
     'green',
@@ -13,37 +11,58 @@ let words = [
     'teal'
 ];
 
-const randWord = words[randIntRange(words.length)];
-
-document.write(`<b>${randWord}: </b>`);
-let i;
-let unsolved = [];
-for(i = 0; i < randWord.length; i++) {
-    unsolved.push('_');
-}
-document.write(unsolved.join(` `));
-
-
+// Classes
 class Word {
     constructor(word) {
         this.startWord = word;
-
+        this.wordArr = word.split('');
+        this.unsolvedArr = this.initArray(this.wordArr);
+        // console.log(this.startWord);
     }
-    createArray() {
-
+    initArray(arr) {
+        let arr2 = [];
+        for(let i = 0; i < arr.length; i++) {
+            arr2.push('_');
+        }
+        return arr2;
+    }
+    checkWord(str) {
+        for(let i = 0; i < this.wordArr.length; i++) {
+            console.log('in loop');
+        }
     }
 }
-let aWord = new Word('apple');
-// console.log(aWord);
+function start() {
+    generateRndmWord();
+    displayText();
+    word.checkWord();
+}
+
+
+// On Start
+let unsolved = document.getElementById('unsolvedWord');
+
+function displayText() {
+    let text = '';
+    for(let i = 0; i < word.unsolvedArr.length; i++) {
+        text += `${word.unsolvedArr[i]} `;
+    }
+    unsolved.innerHTML = text;
+}
+
+function generateRndmWord() {
+    let rndWord = wordList[randIntRange(wordList.length)];
+    word = new Word(rndWord);
+    console.log('Random word generated: ' + word.startWord);
+}
 
 
 
-
-
-
-
+// Utilities
 function randIntRange(range) {
     let rand = Math.floor(Math.random() * range);
-    console.log(rand);
+    // console.log(rand);
     return rand;
 }
+
+start();
